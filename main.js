198ca4f83c5ce6a4dbb87894203265446765e23b
Nick Bakkegaard <nickbakkegaard@gmail.com>
2013-09-11 22:35:42 +0200

    made basic list working

--- main.js
+++ main.js
@@ -1,3 +1,84 @@
+var usernames= new Array("bakkegaard","casper91","gmark","Bettedaniel");
+var persons= new Array();
+
+function update(){
+	persons.sort(function(a,b){return b.count-a.count});
+	console.log(persons);
+
+	var start="<table id=\"table\" class=\"table table-striped\"> <tr> <th>#</th> <th>Name</th> <th>Score</th> </tr>", end="</table>";
+	returnValue= start;
+	for(var i=0;i<persons.length;i++){
+		start+= "<tr>";
+		
+		start+= "<td>";
+		start+= i+1;
+		start+= "</td>";
+
+		start+= "<td>";
+		start+= persons[i]["userName"] + " ("+persons[i]["name"]+")";
+		start+= "</td>";
+
+		start+= "<td>";
+		start+= persons[i]["count"];
+		start+= "</td>";
+		
+		start+="</tr>";
+	}
+
+	start+= end;
+
+	console.log(start);
+
+	$("#table").remove();
+	$("#input").after(start);
+	console.log("done");
+
+}
+
+function buildPerson(data){
+
+	var temp={};
+	temp.name= data["name"];
+	temp.userName= data["uname"];
+	temp.accept= new Array()
+	temp.count= 0;
+	temp.active= true;
+
+	for(var i=0;i< data["subs"].length;i++){
+		if(data["subs"][i][2]===90){
+			if(temp.accept.indexOf(data["subs"][i][1])==-1){
+				temp.accept.push(data["subs"][i][1]);
+				temp.count++;
+			}
+
+		}
+
+	}
+	persons.push(temp);
+	update();
+
+}
+
+function getData(username){
+
+	var s="http://uhunt.felix-halim.net/api/uname2uid/"+ username;
+	$.ajax({
+		url: s
+		}).done(function(data){
+			var s= "http://uhunt.felix-halim.net/api/subs-user/" + data;
+
+			$.ajax({
+			url: s
+			}).done(function ( data ) {
+				buildPerson(data);
+			})
+		})
+};
+
+for(var i=0;i<usernames.length;i++){
+	getData(usernames[i]);
+}
+
 $(function(){
-	$("#main").prepend("hej");
+	$("#main").prepend("<h1>UVA scoreboard</h1>");
 })
